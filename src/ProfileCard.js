e765e5937a0620e8404c4529dc9ec24fd2c25f1f
Manuel Alingog <alingman01@gmail.com>
2024-06-20 18:04:21 -0700

    added Bulma and styled app

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,21 @@
-export default function ProfileCard({ title, handle, image}) {
+export default function ProfileCard({ title, handle, image, description}) {
   return (
-    <div>
-      <img src={image} alt="pda logo"/>
-      <div>The title is {title}</div>
-      <div>The handle is {handle}</div>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-1by1">
+          <img
+            src={image}
+            alt="pda logo"
+          />
+        </figure>
+      </div>
+    <div class="card-content">
+      <div class="media-content">
+        <p class="title is-4">{title}</p>
+        <p class="subtitle is-6">{handle}</p>
+      </div>
+      <div className="content">{description}</div>
     </div>
+  </div>
   )
 }
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import alexaImg from './images/alexa.png'
 import cortanaImg from './images/cortana.png'
@@ -5,10 +6,28 @@ import siriImg from './images/siri.png'
 
 export default function App() {
   return (
-    <>
-    <ProfileCard title="Asana" handle="@alexa99" image={alexaImg}/>
-    <ProfileCard title="Cortana" handle="@cortana32" image={cortanaImg}/>
-    <ProfileCard title="Siri"  handle="@siri01" image={siriImg}/>
-    </>
+    <div>
+      <section className='hero is-primary'>
+        <div className='hero-body'> 
+          <p className='title'>Personal Digital Assitants</p>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is 4'>
+              <ProfileCard title="Asana" handle="@alexa99" image={alexaImg} description="Alexa was created by Amazon"/>  
+            </div>
+            <div className='column is 4'>
+              <ProfileCard title="Cortana" handle="@cortana32" image={cortanaImg} description="Cortana was made by microsoft"/>
+            </div>
+            <div className='column is 4'>
+              <ProfileCard title="Siri"  handle="@siri01" image={siriImg} description="Siri was made by Apple is being phased out"/>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
