9df046223ab8d4574f5e2e4c99d9ce9528fec584
Manuel Alingog <alingman01@gmail.com>
2024-06-23 18:07:38 -0700

    setup animal useState skeleton

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
+function getRandomAnimals() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+
 export default function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    alert("Button clicked");
+    setAnimals([...animals, getRandomAnimals()])
   }
 
   return (
     <div>
       <button onClick={handleClick}>Click button</button>
+      <div>{animals}</div>
     </div>
   )
 }
