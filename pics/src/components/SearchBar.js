24b6487b33834ab72bffd70faaac9923e08b9783
Manuel Alingog <alingman01@gmail.com>
2024-06-30 22:55:40 -0700

    setup useState for input values

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,6 +1,25 @@
-const SearchBar = () => {
+import { useState } from "react";
+
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState('')
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  }
+
   return (
-    <div>SearchBar</div>
+    <div>
+      <form onSubmit={ handleSubmitForm }>
+        <input value={term} onChange={handleChange}/>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
