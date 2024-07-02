b9d9c485240c8791c495bb15c6f56f49c920e75b
Manuel Alingog <alingman01@gmail.com>
2024-07-01 22:07:23 -0700

    implement displayign images from search

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,13 +1,21 @@
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+import searchImages from "./api";
+import { useState } from "react";
 
 const App = () => {
-  const handleSubmit= (term) => {
-    console.log('Do a search with ' + term)
+  const [images, setImages] = useState([])
+
+  const handleSubmit= async(term) => {
+    const images = await searchImages(term);
+
+    setImages(images);
   }
  
   return (
     <div>
       <SearchBar onSubmit={handleSubmit}/>
+      <ImageList images={images} />
     </div>
   )
 }
--- pics/src/components/ImageShow.js
+++ pics/src/components/ImageShow.js
@@ -1,7 +1,8 @@
-const ImageShow = () => {
-  return (
-    <div>ImageShow</div>
-  )
+const ImageShow = ({image}) => {
+  return <div>
+    <img alt={image.alt_description} src={image.urls.small} />
+  </div>
+  
 }
 
 export default ImageShow;
